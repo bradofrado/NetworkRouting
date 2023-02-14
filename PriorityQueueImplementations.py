d4fab3dce8e7f6f029b8a8b6ea330bd44d4329f2
Braydon Jones <bradofrado@gmail.com>
2023-02-13 17:48:26 -0700

    Generalize the heap implememntation

--- PriorityQueueImplementations.py
+++ PriorityQueueImplementations.py
@@ -34,35 +34,8 @@ class HeapPriorityQueue(PriorityQueue):
 		self.heap[0] = None
 		self.switch(0, self.count - 1)
 		self.count -= 1
-		self.delete(0)
+		self.pDown(0)
 		return min
-
-	def delete(self, index):
-		if index * 2 + 1 >= self.count:
-			return
-
-		left = self.heap[index * 2 + 1]
-		right = self.heap[index * 2 + 2] if self.count > index * 2 + 2 else None
-
-		switchIndex = -1
-		if right == None or left[1] < right[1]:
-			switchIndex = index * 2 + 1
-		elif right[1] < left[1]:
-			switchIndex = index * 2 + 2	
-
-		if switchIndex != -1 and self.heap[index][1] > self.heap[switchIndex][1]:
-			self.switch(index, switchIndex)
-			self.delete(switchIndex)
-
-	def switch(self, src, dest):
-		srcKey = self.heap[src]
-		destKey = self.heap[dest]
-		self.heap[src] = destKey
-		self.heap[dest] = srcKey
-		if srcKey != None:
-			self.pointer[srcKey[0]] = dest
-		if destKey != None:
-			self.pointer[destKey[0]] = src
 
 	def decrease_key(self, node, val):
 		index = self.pointer[node.node_id]
@@ -70,21 +43,7 @@ class HeapPriorityQueue(PriorityQueue):
 			return
 
 		self.heap[index][1] = val
-		self.decrease(index)
-	
-	def decrease(self, index):
-		parentKey = self.heap[index // 2] if index > 0 else None
-		if parentKey == None:
-			return
-
-		curr = self.heap[index]
-
-		if curr[1] < parentKey[1]:
-			self.heap[index // 2] = curr
-			self.heap[index] = parentKey
-			self.pointer[parentKey[0]] = index
-			self.pointer[curr[0]] = index // 2
-			self.decrease(index // 2)
+		self.pUp(index)
 
 	def empty(self):
 		return self.count == 0
@@ -99,10 +58,42 @@ class HeapPriorityQueue(PriorityQueue):
 			self.heap[i] = [node.node_id, dist[node.node_id]]
 			self.pointer[node.node_id] = i
 
-			if dist[node.node_id] != float('inf'):
-				temp = self.heap[0]
-				self.heap[0] = self.heap[i]
-				self.heap[i] = temp
-				self.pointer[node.node_id] = 0
-				self.pointer[temp[0]] = i
-		pass
+			self.pUp(i)
+
+	def pUp(self, index):
+		parentKey = self.heap[index // 2] if index > 0 else None
+		if parentKey == None:
+			return
+
+		curr = self.heap[index]
+
+		if curr[1] < parentKey[1]:
+			self.switch(index, index // 2)
+			self.pUp(index // 2)
+
+	def pDown(self, index):
+		if index * 2 + 1 >= self.count:
+			return
+
+		left = self.heap[index * 2 + 1]
+		right = self.heap[index * 2 + 2] if self.count > index * 2 + 2 else None
+
+		switchIndex = -1
+		if right == None or left[1] < right[1]:
+			switchIndex = index * 2 + 1
+		elif right[1] < left[1]:
+			switchIndex = index * 2 + 2	
+
+		if switchIndex != -1 and self.heap[index][1] > self.heap[switchIndex][1]:
+			self.switch(index, switchIndex)
+			self.pDown(switchIndex)
+
+	def switch(self, src, dest):
+		srcKey = self.heap[src]
+		destKey = self.heap[dest]
+		self.heap[src] = destKey
+		self.heap[dest] = srcKey
+		if srcKey != None:
+			self.pointer[srcKey[0]] = dest
+		if destKey != None:
+			self.pointer[destKey[0]] = src
